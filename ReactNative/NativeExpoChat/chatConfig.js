da48e34a4ef1f02adecaf1d54560ca16592ec4e0
Amos Gyamfi <amosgyamfi.fi@gmail.com>
2024-03-18 13:56:09 +0200

    Create chatConfig.js

---
+++ ReactNative/NativeExpoChat/chatConfig.js
@@ -0,0 +1,6 @@
+// chatConfig.js
+// Configs to connect the user to chat
+export const chatApiKey = 'dz5f4d5kzrue'
+export const chatUserId = 'lively-wood-7';
+export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGl2ZWx5LXdvb2QtNyIsImV4cCI6MTcxMDUzMjQ0Nn0.KMkjl68QocLTO_B5E4jIw2s5VBmRLOIvFnZkYNQepmc';
+export const chatUserName = 'lively';
