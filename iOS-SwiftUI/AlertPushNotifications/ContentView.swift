0927cd0206386032f542d3c7caf3ac3aa3cd0edc
Amos Gyamfi <amosgyamfi.fi@gmail.com>
2024-07-26 14:34:38 +0300

    Create ContentView.swift

---
+++ iOS-SwiftUI/AlertPushNotifications/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  AlertPushNotifications
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("**All Hands**: \(Date.now.formatted())")
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
